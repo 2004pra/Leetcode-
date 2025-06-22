fefba65054c7811bf1cf5703b583670bd4118470
Prashant mishra <102900421+2004pra@users.noreply.github.com>
2025-06-22 21:26:28 +0530

    Time: 0 ms (100%), Space: 15.2 MB (32.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,59 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target) return mid;
-            //left half sorted 
-            if(nums[low]<=nums[mid]){
-                     if(nums[low]<=target && target<=nums[mid]){
-                        high = mid-1;
-                     }
-                     else{
-                        low = mid +1;
-                     }
+
+int findKRotation(vector<int> &nums ) {
+        
+      int  start = 0;
+    int  end = nums.size() - 1;
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+
+        
+        if (nums[mid] > nums[end]) {
+            start = mid + 1;
+        } else {
+           
+            end = mid;
+        }
+    }
+
+    
+    return start;
+    }
+
+
+    // binary search function 
+      int bs(vector<int>& nums, int target,int start,int end){
+        int s = nums.size();
+       
+       
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end = mid-1;
             }
             else{
-                if(nums[mid]<=target && target <= nums[high]){
-                    low = mid+1;
-                }
-                else{
-                    high = mid -1;
-                }
+                start = mid +1;
             }
         }
         return -1;
+      }
+
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+       int index = findKRotation(nums);
+        int a1=bs(nums,target,0,index-1);
+        int a2=bs(nums,target,index,n-1);
+         if(a1==-1){
+            return a2;
+         }
+         else{
+            return a1;
+         }
+
     }
 };
