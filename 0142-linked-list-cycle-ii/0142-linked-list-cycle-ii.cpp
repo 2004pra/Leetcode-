2e2374d4a8e1a4a6e99515d734747abe1efa16df
Prashant mishra <102900421+2004pra@users.noreply.github.com>
2025-08-21 22:51:48 +0530

    Time: 7 ms (67.53%), Space: 11.3 MB (83.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -12,35 +12,19 @@ public:
         
          ListNode* slow = head;
          ListNode* fast = head;
-         
-
          while(fast!=NULL && fast->next!=NULL){
                  slow=slow->next;
                  fast= fast->next->next;
-                 
-                 
-                  
-                  if(slow==fast){
-                    
+                  if(slow==fast){  
                     break;
                   }
-                  
-                 
-
-
          }
-
          if(!fast || !fast->next) return NULL;
          slow = head;
          while(fast!=slow){
                 slow=slow->next;
                 fast=fast->next;
-
          }
-         
   return slow;
-   
-  
-
     }
 };
