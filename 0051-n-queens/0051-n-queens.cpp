ba04935e08be58fd4958ab62adf5b85f81c5b447
Prashant mishra <102900421+2004pra@users.noreply.github.com>
2025-12-25 22:36:15 +0530

    Time: 3 ms (62.83%), Space: 10 MB (99.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,58 +1,54 @@
 class Solution {
 public:
 
-bool isSafe(vector<string> &board,int row,int col, int n){
-    //for horizontal 
-    for(int j = 0; j<n;j++){
+bool isSafe(vector<string>& board,int row,int col,int n){
+    //horizontal check 
+    for(int j=0;j<n;j++){
         if(board[row][j]=='Q'){
             return false;
         }
     }
-
-    //for vertical
+    // for vertical
     for(int i=0;i<n;i++){
         if(board[i][col]=='Q'){
             return false;
         }
     }
-
-    //for left diagonal
-
-    for(int i = row,j=col;i>=0 && j>=0 ;i--,j--){
+    // for left diagonal
+    for(int i =row,j=col;i>=0 && j>=0;i--,j--){
         if(board[i][j]=='Q'){
             return false;
         }
     }
-
-//for right diagonal
-    for(int i=row,j=col;i>=0 && j<n;i--,j++){
-          if(board[i][j]=='Q'){
+    //for right diagonal
+    for(int i=row,j=col;i>=0&&j<n;i--,j++){
+        if(board[i][j]=='Q'){
             return false;
-          }
+        }
     }
-
     return true;
 }
 
-void nqueens(vector<string> &board,int row, int n,vector<vector<string>> &ans){
+void helper( vector<string>& board,vector<vector<string>>& ans,int row,int n){
     if(row==n){
-     ans.push_back({board});
-     return;
+         ans.push_back({board});
+         return;
     }
 
-    for(int j = 0;j<n;j++){
-        if(isSafe(board,row,j,n)){
-            board[row][j]='Q';
-            nqueens(board,row+1,n,ans);
-            board[row][j]='.';
-        }
+    for(int i =0;i<n;i++){
+           if(isSafe(board,row,i,n)){
+            board[row][i]='Q';
+            helper(board,ans,row+1,n);
+            board[row][i]='.';
+           }
     }
+
 }
 
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n,string(n,'.'));
         vector<vector<string>> ans;
-        nqueens(board,0,n,ans);
+        helper(board,ans,0,n);
         return ans;
     }
 };
