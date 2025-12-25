d8184ec3f683b1ecee8d02deaab63660f46d5a22
Prashant mishra <102900421+2004pra@users.noreply.github.com>
2025-12-25 20:47:09 +0530

    Time: 0 ms (100%), Space: 13.6 MB (78.05%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -3,17 +3,14 @@ public:
     int arraySign(vector<int>& nums) {
         long long prod = 1;
         for(long long i =0;i<nums.size();i++){
-            prod= (long long)prod*nums[i]; 
-        }
-        if(prod<0){
-            return -1;
-        }
-        else if(prod>0){
-            return 1;
-        }
-        else{
+           if(nums[i]==0){
             return 0;
+           }
+           else if(nums[i]<0){
+            prod = - prod;
+           }
         }
+        return prod;
         
     }
 
