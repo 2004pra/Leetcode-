c255d8d8b8b55fcf24c16482ba3cb5132722cf51
Prashant mishra <102900421+2004pra@users.noreply.github.com>
2026-06-27 19:02:06 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,59 +1,41 @@
 class Solution {
 public:
 
-int findKRotation(vector<int> &nums ) {
-        
-      int  start = 0;
-    int  end = nums.size() - 1;
+   int findKR(vector<int>& nums,int& t){
+           int start = 0;
+           int end = nums.size()-1;
 
-    while (start < end) {
-        int mid = start + (end - start) / 2;
-
-        
-        if (nums[mid] > nums[end]) {
-            start = mid + 1;
-        } else {
-           
-            end = mid;
-        }
-    }
-
-    
-    return start;
-    }
-
-
-    // binary search function 
-      int bs(vector<int>& nums, int target,int start,int end){
-        int s = nums.size();
-       
-       
-        while(start<=end){
+          while(start<end){
             int mid = start + (end-start)/2;
-            if(nums[mid]==target){
-                return mid;
+            if(nums[mid]>nums[end]){
+               start = mid+1;
+            }else{
+                end = mid;
             }
-            else if(nums[mid]>target){
-                end = mid-1;
-            }
-            else{
-                start = mid +1;
-            }
-        }
-        return -1;
-      }
+          }
+          return start;
+   }
 
+   int bs(vector<int>& nums,int& t,int l,int r ){
+    while(l<=r){
+        int mid = l+(r-l)/2;
+        if(nums[mid]==t){
+            return mid;
+        }
+        else if(nums[mid]>t) r = mid-1;
+        else l=mid+1;
+
+       
+    }
+     return -1;
+   }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-       int index = findKRotation(nums);
-        int a1=bs(nums,target,0,index-1);
-        int a2=bs(nums,target,index,n-1);
-         if(a1==-1){
-            return a2;
-         }
-         else{
-            return a1;
-         }
+       int index = findKR(nums,target);
+       // abb 2 alag alag halfs main binary seacrh lagayenge  
+       int a1 = bs(nums,target,0,index-1);
+       int a2 = bs(nums,target,index,nums.size()-1);
+       if(a1==-1) return a2;
+       else return a1;
 
     }
 };
