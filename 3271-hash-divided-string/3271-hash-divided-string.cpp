2fa4aa1dc31464f3a0b854223a449cf385a7b06c
Prashant mishra <102900421+2004pra@users.noreply.github.com>
2026-07-25 21:29:35 +0530

    Time: 1 ms (49.11%), Space: 11.2 MB (35.84%) - LeetHub

---
+++ 3271-hash-divided-string/3271-hash-divided-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string stringHash(string s, int k) {
+        int n = s.size();
+        string  ans = "";
+        for(int i=0;i<n;i+=k){
+            int sum =0;
+            for(int j=i;j<i+k;j++){
+              sum+=s[j]-'a';
+            }
+            ans+=('a'+(sum%26));
+        }
+        return ans;
+    }
+};
